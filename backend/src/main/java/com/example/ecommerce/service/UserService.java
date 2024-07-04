f9c6eedbcc3caabc44f304b513f074f49bba192a
diyabodiwala <83166513+diyabodiwala@users.noreply.github.com>
2024-07-04 22:39:45 +0530

    Create UserService.java

---
+++ backend/src/main/java/com/example/ecommerce/service/UserService.java
@@ -0,0 +1,16 @@
+package com.example.ecommerce.service;
+
+import com.example.ecommerce.model.User;
+import com.example.ecommerce.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+}
