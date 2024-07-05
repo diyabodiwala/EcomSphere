812ffc5849c362430548f04f2d6e90066f86adcb
diyabodiwala <83166513+diyabodiwala@users.noreply.github.com>
2024-07-05 17:03:38 +0530

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Auth from './components/Auth';
+
+function App() {
+  return (
+    <div>
+      <h1>E-commerce Website</h1>
+      <Auth />
+    </div>
+  );
+}
+
+export default App;
